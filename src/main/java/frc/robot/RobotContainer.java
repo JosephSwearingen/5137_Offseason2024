fc3303d779e1d39cb7bcf5e3eb8e9068e49c76cc
JosephSwearingen <jhsweargn@gmail.com>
2024-06-12 17:24:44 -0700

    Created intake

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,13 +6,34 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.CommandPS4Controller;
+import frc.robot.Subsystems.*;
+import frc.robot.Commands.*;
 
 public class RobotContainer {
+  private CommandPS4Controller controller;
+
+  private Intake intake;
+  private Intake_Commands intake_Commands;
+
   public RobotContainer() {
+    controller = new CommandPS4Controller(0);
+
+    intake = new Intake();
+    intake_Commands = new Intake_Commands(intake);
+
     configureBindings();
   }
 
-  private void configureBindings() {}
+  private void configureBindings() {
+    controller.R2()
+    .onTrue(intake_Commands.runForward())
+    .onFalse(intake_Commands.stop());
+
+    controller.L2()
+    .onTrue(intake_Commands.runBackward())
+    .onFalse(intake_Commands.stop());
+  }
 
   public Command getAutonomousCommand() {
     return Commands.print("No autonomous command configured");
